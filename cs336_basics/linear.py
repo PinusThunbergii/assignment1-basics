d19626a98328f9b7b277d83488a7b279af199561
a.maksimishin <a.maksimishin@avto.pro>
2025-06-22 16:19:11 +0300

    12

--- cs336_basics/linear.py
+++ cs336_basics/linear.py
@@ -5,7 +5,12 @@ from einops import rearrange, einsum
 
 class Linear(nn.Module):
     
-    def __init__(self, in_features: int, out_features: int, device: torch.device|None = None, dtype: torch.dtype|None = None):
+    def __init__(self, 
+                in_features: int, 
+                out_features: int, 
+                device: torch.device | None = None,
+                dtype: torch.dtype | None = None):
+        
         super().__init__()
         std = math.sqrt(2.0 / (in_features + out_features))
         w = torch.empty((out_features, in_features), dtype=dtype, device=device)
